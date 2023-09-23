07f08298b1496c4183f6497667e462b92a34ecdb
Edenilson João Pereira <52428137+ninojp@users.noreply.github.com>
2023-09-23 18:21:28 -0300

    instalando a primeira dependência no projeto

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+// Sempre que vamos trabalhar com um projeto em Node.js do zero, uma das primeiras coisas que fazemos
+//é criar um arquivo package.json utilizando o comando npm init ou yarn init; assim como para todas as
+//instalações de libs externas utilizamos o comando npm install <nome do pacote> ou yarn add <nome do pacote>.
+// Para fazer uma instalação global de pacotes, utilizamos os comandos npm install -g <nome do pacote>
+//ou yarn add global <nome do pacote>. A recomendação é que a instalação de pacotes seja feita sempre localmente (sem o -g) 
+//e que a instalação global só seja feita em casos específicos 
+
+// Estes pacotes de código podem ser instalados localmente, estando disponíveis somente para o projeto no qual
+//foi instalado através da pasta node_modules, e globalmente, sendo instalados em um diretório geral do NPM e 
+//ficando disponíveis para todos os projetos em seu computador, sem a necessidade de instalar separadamente em cada projeto.
+
+console.log(chalk.green('ola mundo'));
+console.log('São geralmente recuperados a partir de um objeto [FileList] (https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/');
+console.log('São geralmente recuperados a partir de um objeto [FileList] (https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/');
